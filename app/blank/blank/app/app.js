6376528b9a111e5a43463fae52f505ccc37b3a1d
Serhii Khoma <srghma@gmail.com>
2021-04-07 20:48:57 +0300

    feat: update

--- app/blank/blank/app/app.js
+++ app/blank/blank/app/app.js
@@ -44,9 +44,19 @@ import {
     // TabContentItem,
 } from "@nativescript/core";
 
-// const application = require("tns-core-modules/application");
+// https://github.com/NativeScript/NativeScript/blob/17c85107ba84953630b0471c1f6f3d68f6d59f76/packages/core/ui/builder/index.ts#L36
+// Application.run({
+//     create: () => {
+//         const root = new NSVRoot<View>();
+//         render(app, root, () => console.log(`Container updated!`), "__APP_ROOT__");
 
-// application.run({ moduleName: "app-root" });
+//         return root.baseRef.nativeView;
+//     },
+// });
+
+const application = require("tns-core-modules/application");
+
+application.run({ moduleName: "app-root" });
 
 /*
 Do not place any code after the application has been started as it will not
